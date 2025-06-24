76fa12ff304464ef0e2081b3beac33a87b2b170b
EzhikZhenya1 <churakov.ezhik.zhenya@gmail.com>
2025-06-24 19:19:38 +0300

    Add first, second, third parts, add concat result

--- untitled/src/main/java/Application.java
+++ untitled/src/main/java/Application.java
@@ -1,5 +1,5 @@
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 public class Application {
@@ -10,5 +10,31 @@ public class Application {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.GET, null, String.class);
         System.out.println("All users: " + response.getBody());
+
+        String session = response.getHeaders().getFirst("Set-Cookie");
+        System.out.println("Set-Cookie: " + session);
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", session);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+
+        User user1 = new User(3, "James", "Brown", 22);
+        HttpEntity<User> add = new HttpEntity<>(user1, headers);
+        ResponseEntity<String> addResponse = restTemplate.exchange(URL, HttpMethod.POST, add, String.class);
+        System.out.println("Add user: " + addResponse.getBody());
+
+        User user2 = new User(3, "Thomas", "Shelby", 32);
+        HttpEntity<User> update = new HttpEntity<>(user2, headers);
+        ResponseEntity<String> updateResponse = restTemplate.exchange(URL, HttpMethod.PUT, update, String.class);
+        System.out.println("Update user: " + updateResponse.getBody());
+
+        HttpEntity<User> delete = new HttpEntity<>(headers);
+        ResponseEntity<String> deleteResponse = restTemplate.exchange(URL + "/3", HttpMethod.DELETE, delete, String.class);
+        System.out.println("Delete user: " + deleteResponse.getBody());
+
+        String concated = addResponse.getBody().concat(updateResponse.getBody().concat(deleteResponse.getBody()));
+        System.out.println(concated);
     }
 }
